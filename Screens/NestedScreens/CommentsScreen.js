05c228936f4a169b10c4b800ad0af2fc699aa73a
Stas Korobchenko <stanislav.govoruhin@gmail.com>
2023-02-01 21:16:28 +0200

    com add

--- Screens/NestedScreens/CommentsScreen.js
+++ Screens/NestedScreens/CommentsScreen.js
@@ -1,18 +1,55 @@
 import { useState } from "react";
-import { View, Text } from "react-native";
+import { View, Text,Image, TouchableOpacity,TextInput,StyleSheet } from "react-native";
 import { db } from "../../firebase/config";
+import { doc, addDoc, collection } from "firebase/firestore"
 import { useSelector } from "react-redux";
+import { async } from "@firebase/util";
 
 export const CommentsScreen = ({route}) => {
     const { postId } = route.params;
     const [ comment, setComment ] = useState("");
     const { nickName } = useSelector((state) => state.auth);
 
-    const createPost = () => { };
+    const createPost = async () => {
+        await addDoc(collection(doc(db, "posts", postId), "comments"), {comment, nickName})
+     };
     // console.log(postId)
     return (
-        <View>
+        <View >
             <Text>Hi</Text>
+
+            <View style={{marginTop: "auto"}}>
+                <TextInput style={styles.input } placeholder="Комментировать..." onChangeText={setComment}/>
+            <TouchableOpacity style={styles.sendBtn} activeOpacity={0.7} onPress={createPost}>
+                     <Image source={require('../Images/arrow-top.png')} />
+            </TouchableOpacity>
+            </View>
         </View>
     )
 }
+
+
+const styles = StyleSheet.create({
+    input: {
+        position: 'relative',
+        // justifyContent: 'flex-end',
+        height: 50,
+        paddingLeft: 16,
+        color: "#BDBDBD",
+        backgroundColor: "#F6F6F6",
+        borderRadius: 100,
+        borderWidth: 1,
+        borderColor: "#E8E8E8"
+    },
+    sendBtn: {
+        position: 'absolute',
+        right: 8,
+        bottom: 8,
+        backgroundColor: "#FF6C00",
+        width: 34,
+        height: 34,
+        borderRadius: 100,
+        paddingLeft: 11,
+        paddingTop: 8,
+    },
+})
